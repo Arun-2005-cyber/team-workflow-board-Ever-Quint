95a17e93034ab9e0129feeee06a9c1b3ea26e27f
Arunprakash S <arunprakashsekar2005@gmail.com>
2025-12-11 14:24:31 +0530

    initializing the app layout

--- team-workflow-board/src/main.jsx
+++ team-workflow-board/src/main.jsx
@@ -2,15 +2,15 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import './index.css';
+import '../src/index.css';
 
 // Create root and render the App wrapped in BrowserRouter (for routing/URL filters)
 const container = document.getElementById('root');
 const root = createRoot(container);
 root.render(
-  <React.StrictMode>
+
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>
+ 
 );
