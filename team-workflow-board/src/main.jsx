ce3748add15c3476cbba287d9574f1e4d68b091e
Arunprakash S <arunprakashsekar2005@gmail.com>
2025-12-11 09:31:19 +0530

    creating main.jsx,Stylings

--- team-workflow-board/src/main.jsx
+++ team-workflow-board/src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import './index.css';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// Create root and render the App wrapped in BrowserRouter (for routing/URL filters)
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
